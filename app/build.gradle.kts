1bb4e7b0290b9880cc3751ffcca82f4ddb6a3eda
Narumichi Kubo <narumichi7373@gmail.com>
2024-03-16 16:13:06 +0900

    feat: add github feature module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+    implementation(project(":feature:github"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "AndroidApolloClient"
 include(":app")
+include(":feature:github")
